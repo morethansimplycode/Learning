395edf43a22416c94377cb8b52102771cadef0e2
Oscar <morethansimplycode@gmail.com>
2016-02-25 22:32:34 +0100

    Initial

---
+++ BajoAcoplamiento/BajoAcoplamiento/VentanaReutilizable.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BajoAcoplamiento
+{
+    partial class VentanaReutilizable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.bAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbText
+            // 
+            this.tbText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbText.Location = new System.Drawing.Point(22, 52);
+            this.tbText.Margin = new System.Windows.Forms.Padding(10);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(695, 165);
+            this.tbText.TabIndex = 0;
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Location = new System.Drawing.Point(19, 19);
+            this.lbTitulo.Margin = new System.Windows.Forms.Padding(10);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(43, 16);
+            this.lbTitulo.TabIndex = 1;
+            this.lbTitulo.Text = "Título:";
+            // 
+            // bCancel
+            // 
+            this.bCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCancel.Location = new System.Drawing.Point(634, 232);
+            this.bCancel.Margin = new System.Windows.Forms.Padding(5);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(83, 25);
+            this.bCancel.TabIndex = 2;
+            this.bCancel.Text = "Cancelar";
+            this.bCancel.UseVisualStyleBackColor = true;
+            // 
+            // bAceptar
+            // 
+            this.bAceptar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bAceptar.Location = new System.Drawing.Point(549, 232);
+            this.bAceptar.Margin = new System.Windows.Forms.Padding(5);
+            this.bAceptar.Name = "bAceptar";
+            this.bAceptar.Size = new System.Drawing.Size(75, 25);
+            this.bAceptar.TabIndex = 3;
+            this.bAceptar.Text = "Aceptar";
+            this.bAceptar.UseVisualStyleBackColor = true;
+            // 
+            // VentanaReutilizable
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(736, 273);
+            this.Controls.Add(this.bAceptar);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.tbText);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "VentanaReutilizable";
+            this.Text = "VentanaReutilizable";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.Button bAceptar;
+    }
+}
---
+++ BajoAcoplamiento/BajoAcoplamiento/VentanaReutilizable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BajoAcoplamiento
+{
+    public partial class VentanaReutilizable : Form
+    {
+        public String Text { get; set; }
+        public Func<VentanaReutilizable, Boolean> IsInputValid { get; set; }
+
+        private Action<String> onAccept;
+        public Action<String> OnAccept
+        {
+            get { return onAccept; }
+            set { onAccept = value; }
+        }
+
+        private Action<String> onCancel;
+        public Action<String> OnCancel
+        {
+            get { return onCancel; }
+            set { onCancel = value; }
+        }
+
+        public VentanaReutilizable()
+        {
+            InitializeComponent();
+        }
+    }
+}
